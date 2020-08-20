c13e34b38e1ee2ce23e17869e270a3f16e6b439d
Bram Verstraeten <bramverstraeten@hotmail.com>
2020-08-21 00:10:44 +0200

    Added functionality: The snake can move. The Score and num moves is displayed. If the snake touches the edge it dies

--- snakecore.py
+++ snakecore.py
@@ -6,27 +6,106 @@ Developer: Bram Verstraeten
 
 # --- Imports ----------------------------------------------------------------------------------------------------------
 import arcade
+from enum import Enum
 
 # --- Constants --------------------------------------------------------------------------------------------------------
 SNAKE_SEGMENT_RADIUS = 5
-SNAKE_SEGMENT_SPACING = SNAKE_SEGMENT_RADIUS * 4/3
+
+# --- Class ------------------------------------------------------------------------------------------------------------
+class Direction(Enum):
+    Up = 1
+    Down = 2
+    Left = 3
+    Right = 4
 
 # --- Class ------------------------------------------------------------------------------------------------------------
 class SnakeCore():
     def __init__(self, Gui):
         self.Gui = Gui
+        self.Gui.SetCore(self)
         self.width, self.height = self.Gui.GetScreenSize()
+        self.CalcGrid()
+
         self.bodyLength = 3
         self.body = self.GenerateBody()
+        self.direction = Direction.Right
+
+        self.score = 0
+        self.moves = 0
+
         self.UpdateGui()
+        arcade.schedule( self.move, 1/20 )
+        self.paused = False
+        self.dead = False
+        
+
+    def CalcGrid(self):
+        self.width = self.width / SNAKE_SEGMENT_RADIUS
+        self.height = self.height / SNAKE_SEGMENT_RADIUS
 
     def GenerateBody(self):
         body = []
         while len(body) < self.bodyLength:
-            coor = [self.width/2 - len(body)*SNAKE_SEGMENT_SPACING, self.height/2]
+            coor = [self.width/2 - len(body), self.height/2]
             body.append(coor)
         return body
         
     def UpdateGui(self):
         self.Gui.SetBody(self.body)
+        self.Gui.SetScore(self.score)
+        self.Gui.SetMoves(self.moves)
         self.Gui.SetSnakeSegmentRadius(SNAKE_SEGMENT_RADIUS)
+
+    def move(self, time):
+        if not self.paused and not self.dead:
+            new_body = []
+            if self.direction == Direction.Right:
+                new_body.append( [ self.body[0][0]+1, self.body[0][1] ] )
+            elif self.direction == Direction.Left:
+                new_body.append( [ self.body[0][0]-1, self.body[0][1] ] )
+            elif self.direction == Direction.Up:
+                new_body.append( [ self.body[0][0], self.body[0][1]+1 ] )
+            elif self.direction == Direction.Down:
+                new_body.append( [ self.body[0][0], self.body[0][1]-1 ] )
+
+            for i, _ in enumerate( self.body ):
+                if not i == 0:
+                    new_body.append( self.body[i-1] )
+
+            if new_body[0][0] <= 0 or new_body [0][0] >= self.width:
+                new_body = self.body
+                self.YouDied()
+
+            if new_body[0][1] <= 0 or new_body [0][1] >= self.height:
+                new_body = self.body
+                self.YouDied()
+
+            self.moves += 1
+
+            self.body = new_body
+        self.UpdateGui()
+
+    def KeyPressed(self, key):
+        if key == arcade.key.UP and not self.direction == Direction.Down:
+            self.direction = Direction.Up
+        elif key == arcade.key.DOWN and not self.direction == Direction.Up:
+            self.direction = Direction.Down
+        elif key == arcade.key.RIGHT and not self.direction == Direction.Left:
+            self.direction = Direction.Right
+        elif key == arcade.key.LEFT and not self.direction == Direction.Right:
+            self.direction = Direction.Left
+
+        if key == arcade.key.P:
+            self.paused = False if self.paused else True
+
+        if key == arcade.key.ENTER:
+            self.score = 0
+            self.moves = 0
+            self.body = self.GenerateBody()
+            self.direction = Direction.Right
+            self.dead = False
+            self.Gui.SetDead()
+
+    def YouDied(self):
+        self.dead = True
+        self.Gui.SetDead()
--- snakegui.py
+++ snakegui.py
@@ -11,19 +11,32 @@ import arcade
 SCREEN_TITLE = "Snake"
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
+SCREEN_HEADER = 50
 
 # --- Class ------------------------------------------------------------------------------------------------------------
 class SnakeGui(arcade.Window):
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT + SCREEN_HEADER, SCREEN_TITLE)
         arcade.set_background_color(arcade.color.BLACK_OLIVE) 
 
         self.body = [[0,0]]
+        self.score = 0
+        self.moves = 0
         self.Snake_Segment_Radius = 0
+        self.dead = False
         
     def on_draw(self):
         # Clear the screen and start drawing
         arcade.start_render()
+
+        # Draw horizontal line
+        arcade.draw_line( 0, SCREEN_HEIGHT+1, SCREEN_WIDTH, SCREEN_HEIGHT+1, arcade.color.DUTCH_WHITE, 2 )
+
+        # Draw text fields
+        arcade.draw_text("Score = " + str(self.score), 10, SCREEN_HEIGHT + SCREEN_HEADER/4, arcade.color.WHITE_SMOKE, 24)
+        arcade.draw_text("Num moves = " + str(self.moves), 10+SCREEN_WIDTH/2, SCREEN_HEIGHT + SCREEN_HEADER/4, arcade.color.WHITE_SMOKE, 24)
+
+        self.FlagDead()
 
         # Draw snake body
         self.draw_body()
@@ -35,7 +48,7 @@ class SnakeGui(arcade.Window):
                 if i == 0:
                     color = arcade.color.WILD_WATERMELON
                 arcade.draw_circle_filled(
-                    coordinates[0], coordinates[1], self.Snake_Segment_Radius, color
+                    coordinates[0]*self.Snake_Segment_Radius, coordinates[1]*self.Snake_Segment_Radius, self.Snake_Segment_Radius, color
                 )
         except:
             print("An error occured when rendering the snake body.")
@@ -52,5 +65,21 @@ class SnakeGui(arcade.Window):
     def SetSnakeSegmentRadius(self, x):
         self.Snake_Segment_Radius = x
 
+    def SetCore(self, core):
+        self.Core = core
+
+    def SetScore(self, score):
+        self.score = score
+
+    def SetMoves(self, moves):
+        self.moves = moves
+
+    def SetDead(self):
+        self.dead = False if self.dead else True
+
     def GetScreenSize(self):
         return SCREEN_WIDTH, SCREEN_HEIGHT
+
+    def FlagDead(self):
+        if self.dead:
+            arcade.draw_text("YOU DIED!", 0, SCREEN_HEIGHT/2, arcade.color.WHITE_SMOKE, 72, width=SCREEN_WIDTH, align="center")
