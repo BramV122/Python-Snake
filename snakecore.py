347231d8b9ca1efdc7f11555ef48769a201dfe2c
Bram Verstraeten <bramverstraeten@hotmail.com>
2020-08-22 14:18:38 +0200

    First version complete. Refactoring needed to make more clean code

--- snakecore.py
+++ snakecore.py
@@ -7,9 +7,11 @@ Developer: Bram Verstraeten
 # --- Imports ----------------------------------------------------------------------------------------------------------
 import arcade
 from enum import Enum
+import random
+import math
 
 # --- Constants --------------------------------------------------------------------------------------------------------
-SNAKE_SEGMENT_RADIUS = 5
+SNAKE_SEGMENT_RADIUS = 10
 
 # --- Class ------------------------------------------------------------------------------------------------------------
 class Direction(Enum):
@@ -23,6 +25,7 @@ class SnakeCore():
     def __init__(self, Gui):
         self.Gui = Gui
         self.Gui.SetCore(self)
+        self.Gui.SetSnakeSegmentRadius(SNAKE_SEGMENT_RADIUS)
         self.width, self.height = self.Gui.GetScreenSize()
         self.CalcGrid()
 
@@ -32,21 +35,24 @@ class SnakeCore():
 
         self.score = 0
         self.moves = 0
+        self.CurrentFoodLoc = None
+        self.AddSegment = False
 
         self.UpdateGui()
-        arcade.schedule( self.move, 1/20 )
+        arcade.schedule( self.move, 1/15 )
         self.paused = False
         self.dead = False
-        
+
+        arcade.schedule( self.SpawnFood, random.random() * 2 )
 
     def CalcGrid(self):
-        self.width = self.width / SNAKE_SEGMENT_RADIUS
-        self.height = self.height / SNAKE_SEGMENT_RADIUS
+        self.width = (self.width - 2*SNAKE_SEGMENT_RADIUS) / SNAKE_SEGMENT_RADIUS
+        self.height = (self.height - 2*SNAKE_SEGMENT_RADIUS) / SNAKE_SEGMENT_RADIUS
 
     def GenerateBody(self):
         body = []
         while len(body) < self.bodyLength:
-            coor = [self.width/2 - len(body), self.height/2]
+            coor = [int(self.width/2) - len(body), int(self.height/2)]
             body.append(coor)
         return body
         
@@ -54,9 +60,9 @@ class SnakeCore():
         self.Gui.SetBody(self.body)
         self.Gui.SetScore(self.score)
         self.Gui.SetMoves(self.moves)
-        self.Gui.SetSnakeSegmentRadius(SNAKE_SEGMENT_RADIUS)
+        self.Gui.SetFood(self.CurrentFoodLoc)
 
-    def move(self, time):
+    def move(self, _):
         if not self.paused and not self.dead:
             new_body = []
             if self.direction == Direction.Right:
@@ -72,17 +78,23 @@ class SnakeCore():
                 if not i == 0:
                     new_body.append( self.body[i-1] )
 
-            if new_body[0][0] <= 0 or new_body [0][0] >= self.width:
+            if self.AddSegment:
+                new_body.append( self.body[-1] )
+                self.AddSegment = False
+
+            if new_body[0][0] < 0 or new_body [0][0] > self.width:
                 new_body = self.body
                 self.YouDied()
 
-            if new_body[0][1] <= 0 or new_body [0][1] >= self.height:
+            if new_body[0][1] < 0 or new_body [0][1] > self.height:
                 new_body = self.body
                 self.YouDied()
 
             self.moves += 1
 
             self.body = new_body
+            self.BodyCollisionDetecion()
+            self.CheckFoodEaten()
         self.UpdateGui()
 
     def KeyPressed(self, key):
@@ -97,6 +109,7 @@ class SnakeCore():
 
         if key == arcade.key.P:
             self.paused = False if self.paused else True
+            self.Gui.SetPaused()
 
         if key == arcade.key.ENTER:
             self.score = 0
@@ -109,3 +122,43 @@ class SnakeCore():
     def YouDied(self):
         self.dead = True
         self.Gui.SetDead()
+
+    def BodyCollisionDetecion(self):
+        head = self.body[0]
+        if head in self.body[1:]:
+            self.YouDied()
+
+    def SpawnFood(self, _):
+        x, y = self.GetRandomCoor()
+        self.CurrentFoodLoc = [x, y]
+        self.UpdateGui()
+        arcade.unschedule(self.SpawnFood)
+
+    def GetRandomCoor(self):
+        x = random.randint(0, self.width)
+        y = random.randint(0, self.height)
+
+        if [x, y] in self.body and self.CalcDistanceBetweenPoints(self.body[0], [x,y]) < 10:
+            x, y = self.GetRandomCoor()
+
+        return x, y
+
+    def CalcDistanceBetweenPoints(self, point1, point2):
+        x_dist = abs( point1[0] - point2[0] )
+        y_dist = abs( point1[1] - point2[1] )
+
+        x_dist_pow = math.pow( x_dist, 2 )
+        y_dist_pow = math.pow( y_dist, 2 )
+        dist = math.sqrt( x_dist_pow + y_dist_pow )
+
+        return int( dist )
+
+    def CheckFoodEaten(self):
+        head = self.body[0]
+
+        if self.CurrentFoodLoc == head:
+            self.score += 1
+            self.CurrentFoodLoc = None
+            self.UpdateGui()
+            arcade.schedule( self.SpawnFood, random.random() * 2 )
+            self.AddSegment = True
--- snakegui.py
+++ snakegui.py
@@ -24,6 +24,8 @@ class SnakeGui(arcade.Window):
         self.moves = 0
         self.Snake_Segment_Radius = 0
         self.dead = False
+        self.paused = False
+        self.food = None
         
     def on_draw(self):
         # Clear the screen and start drawing
@@ -36,19 +38,26 @@ class SnakeGui(arcade.Window):
         arcade.draw_text("Score = " + str(self.score), 10, SCREEN_HEIGHT + SCREEN_HEADER/4, arcade.color.WHITE_SMOKE, 24)
         arcade.draw_text("Num moves = " + str(self.moves), 10+SCREEN_WIDTH/2, SCREEN_HEIGHT + SCREEN_HEADER/4, arcade.color.WHITE_SMOKE, 24)
 
+        #Display if the game is paused or the player is dead
+        self.FlagPaused()
         self.FlagDead()
 
         # Draw snake body
         self.draw_body()
+
+        # Draw food
+        self.draw_food()
 
     def draw_body(self):
         try:
             for i, coordinates in enumerate( self.body ):
                 color = arcade.color.WHITE_SMOKE
                 if i == 0:
-                    color = arcade.color.WILD_WATERMELON
+                    color = arcade.color.GRANNY_SMITH_APPLE
                 arcade.draw_circle_filled(
-                    coordinates[0]*self.Snake_Segment_Radius, coordinates[1]*self.Snake_Segment_Radius, self.Snake_Segment_Radius, color
+                    coordinates[0]*self.Snake_Segment_Radius + self.Snake_Segment_Radius, 
+                    coordinates[1]*self.Snake_Segment_Radius + self.Snake_Segment_Radius,
+                    self.Snake_Segment_Radius/2, color
                 )
         except:
             print("An error occured when rendering the snake body.")
@@ -77,9 +86,27 @@ class SnakeGui(arcade.Window):
     def SetDead(self):
         self.dead = False if self.dead else True
 
+    def SetPaused(self):
+        self.paused = False if self.paused else True
+
     def GetScreenSize(self):
         return SCREEN_WIDTH, SCREEN_HEIGHT
 
     def FlagDead(self):
         if self.dead:
             arcade.draw_text("YOU DIED!", 0, SCREEN_HEIGHT/2, arcade.color.WHITE_SMOKE, 72, width=SCREEN_WIDTH, align="center")
+
+    def FlagPaused(self):
+        if self.paused:
+            arcade.draw_text("GAME PAUSED!", 0, SCREEN_HEIGHT/2, arcade.color.WHITE_SMOKE, 72, width=SCREEN_WIDTH, align="center")
+
+    def SetFood(self, point):
+        self.food = point
+
+    def draw_food(self):
+        if not self.food == None:
+            arcade.draw_circle_filled(
+                    self.food[0]*self.Snake_Segment_Radius + self.Snake_Segment_Radius, 
+                    self.food[1]*self.Snake_Segment_Radius + self.Snake_Segment_Radius, 
+                    self.Snake_Segment_Radius/2, arcade.color.WILD_WATERMELON
+                )
